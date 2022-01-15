84488582129b81b131dac0c4392bf9a165f912a9
LucasMoskun <contact@lucasmoskun.com>
2022-01-15 22:38:10 +0000

    initial functionality authorize test

--- test/test_encode_eth.js
+++ test/test_encode_eth.js
@@ -4,5 +4,20 @@ describe("Test Encode Eth", function() {
   this.timeout(800000)
 
   it("Testing encode eth", async function() {
+
+    const owner = await ethers.getSigner(0);
+    console.log("Getting ABI...");
+    const abi = await ethers.getContractFactory("AuthorizeCreepCoin");
+    console.log("Connecting to contract");
+
+    const contractAddress = "0xb205E9533605A8c3d2bAfA40037B05D3Ed87DF7d";
+    const contract = await abi.attach(contractAddress);
+
+    console.log("Authorizing... ");
+
+    const authTx = await contract.AuthorizeCreepCoinBridge(3,4, owner.address);
+
+    console.log("Finished");
+
   })
 })
