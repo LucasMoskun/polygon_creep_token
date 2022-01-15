9a41cfef1c463759a2695c59e04fb5c5aec734a3
LucasMoskun <contact@lucasmoskun.com>
2022-01-15 18:07:52 +0000

    updated local packages

--- test/test_deploy.js
+++ test/test_deploy.js
@@ -1,14 +1,15 @@
-
+const { ethers } = require("hardhat");
 describe("Fx Portal Deploy", function() {
   this.timeout(800000)
+
   it("Deploying fx portal contract", async function() {
     console.log("Starting...")
 
     const owner = await ethers.getSigner(0);
     console.log("Getting ABI");
-    const abi = await ethers.getContractFactory("FxStateRootTunnel.sol")
+    const abi = await ethers.getContractFactory("FxStateRootTunnel")
     console.log("Deploying contract...");
-    const contract = await token.deploy();
+    const contract = await abi.deploy();
     console.log("Contract addres: " + contract.address);
     console.log("transaction hash: " + contract.deployTransaction.hash);
     await contract.deployed();
