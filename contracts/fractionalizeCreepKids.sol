96fda0dcf3d4381043288ec9442dfc7c27a5b7d8
Khalid Elassaad <khalidelassaad@gmail.com>
2022-01-15 12:49:14 -0800

    code: one more checkpoint

--- contracts/fractionalizeCreepKids.sol
+++ contracts/fractionalizeCreepKids.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract fractionalizeCreepKids {
+contract fractionalizeCreepKids{
     struct CoinHolder {
         // 1 struct per address, contains mapping of tokenIDs to coin count,
         // and list of TokenIDs held
@@ -18,15 +18,41 @@ contract fractionalizeCreepKids {
         // We could also eliminate TokenIDsHeld and loop through all 1000, test for performance
     }
 
-    string metadataUri;
+    string constant metadataUri = "ipfs://QmcrVNTcC9DTGia2YbdrYchzt26te94DkEGikPNd3q1Ug3";
     address CreepKidsNFTSmartContractAddress;
     mapping(address=>CoinHolder) public addressToCoinHolder;
     mapping(uint=>address) public TokenIDToAuthorizedMinterAddress;
+    mapping(uint=>uint) public TokenIDToURISuffix;
     mapping(uint=>bool) public TokenIDtoAreFractionalizedCoinsMinted;
 
     constructor () {
         CreepKidsNFTSmartContractAddress = 0x7ef232E01C45377b0321ff11cA50c59C5B69212b;
-        metadataUri = "ipfs://QmcrVNTcC9DTGia2YbdrYchzt26te94DkEGikPNd3q1Ug3";
+    }
+
+    function _uintToString(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (_i != 0) {
+            k = k-1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+
+    function getURI(uint TokenID) public pure returns (string memory) {
+        return string(abi.encodePacked(metadataUri, "/", _uintToString(TokenID)));
     }
 
     function _checkValidTokenID (
